4ff44aec0245de46267a61e082644a4d062483ca
LAPTOP-R0U9QQBC\Mediv01 <>
2022-01-11 00:19:00 +0800

    大地图9.1 XML变动

---
+++ RFC Dawn of Civilization/Assets/XML/Text/text_corporations.lua
@@ -0,0 +1,195 @@
+return {
+	TXT_KEY_CORPORATION_INFLUENCES_0 = {
+[[Additional Requirements:
+	[ICON_BULLET]Can only spread to cities on the historical route
+	[ICON_BULLET]Cannot spread after 1500 AD
+Attracted by:
+	[ICON_BULLET]Slaves and additional company resources (especially Gold resources)
+	[ICON_BULLET]Adopting the Merchant Trade or Free Enterprise civics
+	[ICON_BULLET]Location is in Subsaharan and Northeast Africa
+	[ICON_BULLET]Markets and Stables
+	[ICON_BULLET]Trade routes
+	[ICON_BULLET]Coastal Cities
+Deterred by:
+	[ICON_BULLET]Catholicism and Protestantism
+	[ICON_BULLET]Too many other companies present
+]],
+[[附加要求：
+	[ICON_BULLET]只能传播到历史路线上的城市
+	[ICON_BULLET]公元1500年之前
+有利条件:
+	[ICON_BULLET]奴隶和黄金资源
+	[ICON_BULLET]批发贸易或自由市场政策
+	[ICON_BULLET]城市位于中亚或波斯
+	[ICON_BULLET]市场和马厩
+	[ICON_BULLET]贸易路线数量
+不利条件：
+	[ICON_BULLET]城市位于中国
+	[ICON_BULLET]沿海城市没有港口
+	[ICON_BULLET]基督教国家宗教[NEWLINE]
+	[ICON_BULLET]其他公司竞争
+]]
+	},
+
+	TXT_KEY_CORPORATION_TEXTILE_INDUSTRY = {
+		"Textile Industry",
+		"棉纺工业",
+	}
+
+	TXT_KEY_CORPORATION_INFLUENCES_4 = {
+[[Attracted by:
+	[ICON_BULLET]Additional company resources
+	[ICON_BULLET]Adopting the Free Enterprise civic
+	[ICON_BULLET]Granaries, Sewers, Supermarkets
+	[ICON_BULLET]Trade routes
+Deterred by:
+	[ICON_BULLET]Competition with the Fishing Industry company
+	[ICON_BULLET]Too many other companies present
+Side effects:
+	[ICON_BULLET]Can cause [ICON_UNHAPPY] in your cities
+]],
+[[附加要求：
+	[ICON_BULLET]公元1920年之前
+吸引了:
+	[ICON_BULLET]附加公司资源
+	[ICON_BULLET]采用免费企业
+	[ICON_BULLET]市场后无法传播，Weavers，工厂
+	[ICON_BULLET]贸易路线
+受阻于：
+	[ICON_BULLET]与钢铁工业公司的竞争
+	[ICON_BULLET]其他公司竞争
+副作用：
+	[ICON_BULLET]可能导致[ICON_UNHAPPY]和[图标不健康]在你的城市
+]],
+	},
+
+	TXT_KEY_CORPORATION_INFLUENCES_1 = {
+[[Additional Requirements:
+[ICON_BULLET]Can only spread to cities on the historical route[NEWLINE][ICON_BULLET]Cannot spread after 1500 AD[NEWLINE][NEWLINE]Attracted by:[NEWLINE][ICON_BULLET]Additional company resources (especially Spice resources)[NEWLINE][ICON_BULLET]Adopting the Merchant Trade or Free Enterprise civics[NEWLINE][ICON_BULLET]Location is in South East Asia or the Deccan[NEWLINE][ICON_BULLET]Markets and Wharfs[NEWLINE][ICON_BULLET]Trade routes[NEWLINE][NEWLINE]Deterred by:[NEWLINE][ICON_BULLET]Coastal cities without a Harbor[NEWLINE][ICON_BULLET]Christian state religion[NEWLINE][ICON_BULLET]Too many other companies present]]
+	}
+}
+
+
+		<Tag></Tag>
+		<English></English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>附加要求：[NEWLINE][ICON_BULLET]只能传播到属于西班牙、法国、英国、葡萄牙、荷兰或维京文明的城市[NEWLINE][ICON_BULLET]不能传播到公元1800年以后[NEWLINE][ICON_BULLET]只能传播到加勒比、撒哈拉以南非洲的城市，南亚或拥有贸易公司国家奇观[NEWLINE][ICON_BULLET]的城市只能扩展到沿海城市[NEWLINE][NEWLINE]吸引的是：[NEWLINE][ICON_BULLET]其他公司资源[NEWLINE][ICON_BULLET]采用殖民主义或自由市场政体[NEWLINE][ICON_BULLET]是荷兰人文明[NEWLINE][图标符号]港口、海关、银行、贸易公司（国家奇迹）[NEWLINE][图标符号]加勒比海城市[NEWLINE][图标符号]贸易路线[NEWLINE][NEWLINE]被：[NEWLINE][图标符号]太多其他公司阻止</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_INFLUENCES_6</Tag>
+		<English>Additional Requirements:[NEWLINE][ICON_BULLET]Cannot spread after 1920 AD[NEWLINE][NEWLINE]Attracted by:[NEWLINE][ICON_BULLET]Additional company resources[NEWLINE][ICON_BULLET]Adopting the Free Enterprise civic[NEWLINE][ICON_BULLET]Markets, Weavers, Factories[NEWLINE][ICON_BULLET]Trade routes[NEWLINE][NEWLINE]Deterred by:[NEWLINE][ICON_BULLET]Competition with the Steel Industry company[NEWLINE][ICON_BULLET]Too many other companies present[NEWLINE][NEWLINE]Side effects:[NEWLINE][ICON_BULLET]Can cause [ICON_UNHAPPY] and [ICON_UNHEALTHY] in your cities</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>吸引它的是：[NEWLINE][ICON_BULLET]其他公司资源[NEWLINE][ICON_BULLET]采用免费企业公民[NEWLINE][ICON_BULLET]银行、酒厂、工业园区、股票市场[NEWLINE][ICON_BULLET]贸易路线[NEWLINE][NEWLINE]也被[NEWLINE][ICON_BULLET]与计算机行业公司[NEWLINE][ICON_BULLET]的竞争吓倒许多其他公司也会出现[NEWLINE][NEWLINE]的副作用：[NEWLINE][ICON_BULLET]会导致[ICON_UNHAPPY]和[ICON_UNHEALTHY]在你的城市</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_INFLUENCES_7</Tag>
+		<English>Attracted by:[NEWLINE][ICON_BULLET]Additional company resources[NEWLINE][ICON_BULLET]Adopting the Free Enterprise civic[NEWLINE][ICON_BULLET]Factories, Coal Plants, Industrial Parks, Ironworks[NEWLINE][ICON_BULLET]Trade routes[NEWLINE][NEWLINE]Deterred by:[NEWLINE][ICON_BULLET]Competition with the Textile Industry company[NEWLINE][ICON_BULLET]Too many other companies present[NEWLINE][NEWLINE]Side effects:[NEWLINE][ICON_BULLET]Can cause [ICON_UNHAPPY] and [ICON_UNHEALTHY] in your cities</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>吸引它的是：[NEWLINE][ICON_BULLET]其他公司资源[NEWLINE][ICON_BULLET]采用免费的企业公民[NEWLINE][ICON_BULLET]工厂、纺织厂、百货公司，国家美术馆[NEWLINE][ICON_BULLET]贸易路线[NEWLINE][NEWLINE]受到以下因素的阻碍：[NEWLINE][ICON_BULLET]太多其他公司出现[NEWLINE][NEWLINE]副作用：[NEWLINE][ICON_BULLET]可能会导致[ICON_UNHAPPY]和[ICON_UNHEALTHY]</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_CEREAL_INDUSTRY</Tag>
+		<English>Cereal Industry</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>谷物工业</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_INFLUENCES_9</Tag>
+		<English>Attracted by:[NEWLINE][ICON_BULLET]Additional company resources[NEWLINE][ICON_BULLET]Adopting the Free Enterprise civic[NEWLINE][ICON_BULLET]Factories, Weavers, Department Stores, National Gallery[NEWLINE][ICON_BULLET]Trade routes[NEWLINE][NEWLINE]Deterred by:[NEWLINE][ICON_BULLET]Too many other companies present[NEWLINE][NEWLINE]Side effects:[NEWLINE][ICON_BULLET]Can cause [ICON_UNHAPPY] and [ICON_UNHEALTHY] in your cities</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>吸引了：[NEWLINE][ICON_BULLET]更多的公司资源[NEWLINE][ICON_BULLET]采用自由企业公民[NEWLINE][ICON_BULLET]工厂、织布工、百货公司，国家美术馆[NEWLINE][ICON_BULLET]贸易路线[NEWLINE][NEWLINE]被[NEWLINE][ICON_BULLET]太多其他公司阻止[NEWLINE][ICON_BULLET]副作用：[NEWLINE][ICON_BULLET]可能会导致[ICON_UNHAPPY]和[ICON_UNHEALTHY] </Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_SILK_ROUTE</Tag>
+		<English>Silk Route</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>丝绸之路</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_INFLUENCES_5</Tag>
+		<English>Additional Requirements:[NEWLINE][ICON_BULLET]Can only spread into coastal cities[NEWLINE][NEWLINE]Attracted by:[NEWLINE][ICON_BULLET]Additional company resources[NEWLINE][ICON_BULLET]Adopting the Free Enterprise civic[NEWLINE][ICON_BULLET]Lighthouses, Harbors, Supermarkets[NEWLINE][ICON_BULLET]Trade routes[NEWLINE][NEWLINE]Deterred by:[NEWLINE][ICON_BULLET]Competition with the Cereal Industry company[NEWLINE][ICON_BULLET]Too many other companies present[NEWLINE][NEWLINE]Side effects:[NEWLINE][ICON_BULLET]Can cause [ICON_UNHAPPY] in your cities</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>吸引它的是：[NEWLINE][ICON_BULLET]额外的公司资源[NEWLINE][ICON_BULLET]采用免费企业civic[NEWLINE][ICON_BULLET]工厂、煤炭厂、工业园区、钢铁厂[NEWLINE][ICON_BULLET]贸易路线[NEWLINE][NEWLINE]也被[NEWLINE][ICON_BULLET]与纺织工业公司[NEWLINE][ICON_BULLET]的竞争吓倒许多其他公司也会出现[NEWLINE][NEWLINE]的副作用：[NEWLINE][ICON_BULLET]会导致[ICON_UNHAPPY]和[ICON_UNHEALTHY]在你的城市</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_FISHING_INDUSTRY</Tag>
+		<English>Fishing Industry</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>渔业</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_COMPUTER_INDUSTRY</Tag>
+		<English>Computer Industry</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>计算机工业</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_INFLUENCES_2</Tag>
+		<English>Additional Requirements:[NEWLINE][ICON_BULLET]Can only spread to cities on the historical route[NEWLINE][ICON_BULLET]Cannot spread after 1500 AD[NEWLINE][NEWLINE]Attracted by:[NEWLINE][ICON_BULLET]Additional company resources (especially Silk resources)[NEWLINE][ICON_BULLET]Adopting the Merchant Trade or Free Enterprise civics[NEWLINE][ICON_BULLET]Location is in Central Asia or Persia[NEWLINE][ICON_BULLET]Markets and Stables[NEWLINE][ICON_BULLET]Trade routes[NEWLINE][NEWLINE]Deterred by:[NEWLINE][ICON_BULLET]Location is in China[NEWLINE][ICON_BULLET]Coastal cities without a Harbor[NEWLINE][ICON_BULLET]Christian state religion[NEWLINE][ICON_BULLET]Too many other companies present</English>
+		<L1/><L2/><L3/><L4/>
+	[[吸引它的是：
+		[ICON_BULLET]其他公司资源
+		[ICON_BULLET]采用自由市场政体
+		[ICON_BULLET]粮仓、下水道、超市
+		[ICON_BULLET]贸易路线
+	阻止了:
+		与渔业公司[NEWLINE][ICON_BULLET]的竞争太多其他公司出现[NEWLINE][NEWLINE]副作用：[NEWLINE][ICON_BULLET]会导致您所在城市的[ICON_UNHAPPY]</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_LUXURY_INDUSTRY</Tag>
+		<English>Luxury Industry</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>奢侈品行业</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_INFLUENCES_3</Tag>
+		<English>Additional Requirements:[NEWLINE][ICON_BULLET]Can only spread to cities belonging to the Spanish, French, English, Portuguese, Dutch or Viking civilizations[NEWLINE][ICON_BULLET]Cannot spread after 1800 AD[NEWLINE][ICON_BULLET]Can only spread to cities in the Caribbean, Sub-Saharan Africa, South Asia or cities that have the Trading Corporation National Wonder[NEWLINE][ICON_BULLET]Can only spread into coastal cities[NEWLINE][NEWLINE]Attracted by:[NEWLINE][ICON_BULLET]Additional company resources[NEWLINE][ICON_BULLET]Adopting the Colonialism or Free Enterprise civic[NEWLINE][ICON_BULLET]Being the Dutch civilization[NEWLINE][ICON_BULLET]Harbors, Customs Houses, Banks, Trading Company (National Wonder)[NEWLINE][ICON_BULLET]Cities in the Caribbean[NEWLINE][ICON_BULLET]Trade routes[NEWLINE][NEWLINE]Deterred by:[NEWLINE][ICON_BULLET]Too many other companies present</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>附加要求：[NEWLINE][ICON_BULLET]只能扩展到沿海城市[NEWLINE][NEWLINE]吸引的是：[NEWLINE][ICON_BULLET]附加公司资源[NEWLINE][ICON_BULLET]采用免费企业公民[NEWLINE][ICON_BULLET]灯塔、港口，超市[NEWLINE][ICON_BULLET]贸易路线[NEWLINE][NEWLINE]受阻于：[NEWLINE][ICON_BULLET]与谷物工业公司的竞争[NEWLINE][ICON_BULLET]太多其他公司出现[NEWLINE][NEWLINE]副作用：[NEWLINE][ICON_BULLET]可能导致[ICON_UNHAPPY]在你的城市</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_OIL_INDUSTRY</Tag>
+		<English>Oil Industry</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>石油工业</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_TRADING_COMPANY</Tag>
+		<English>Trading Company</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>贸易公司</Chinese>
+		},
+
+		TXT_KEY_CORPORATION_TRANS_SAHARAN_ROUTE = {
+			"Trans Saharan Route",
+			"撒哈拉商路",
+		},
+
+		TXT_KEY_CORPORATION_STEEL_INDUSTRY = {
+			"Steel Industry",
+			"钢铁工业",
+		},
+
+		TXT_KEY_CORPORATION_INFLUENCES_8 = {
+		<English>Attracted by:[NEWLINE][ICON_BULLET]Additional company resources[NEWLINE][ICON_BULLET]Adopting the Free Enterprise civic[NEWLINE][ICON_BULLET]Banks, Distilleries, Industrial Parks, Stock Market[NEWLINE][ICON_BULLET]Trade routes[NEWLINE][NEWLINE]Deterred by:[NEWLINE][ICON_BULLET]Competition with the Computer Industry company[NEWLINE][ICON_BULLET]Too many other companies present[NEWLINE][NEWLINE]Side effects:[NEWLINE][ICON_BULLET]Can cause [ICON_UNHAPPY] and [ICON_UNHEALTHY] in your cities</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>吸引它的是：[NEWLINE][ICON_BULLET]更多的公司资源[NEWLINE][ICON_BULLET]采用免费企业civic[NEWLINE][ICON_BULLET]工厂、实验室、大学，欧洲核子研究中心综合研究中心[NEWLINE][ICON_BULLET]贸易路线[NEWLINE][NEWLINE]被[NEWLINE][ICON_BULLET]与石油工业公司的竞争[NEWLINE][ICON_BULLET]吓倒了太多其他公司出现[NEWLINE][NEWLINE]副作用：[NEWLINE][ICON_BULLET]可能导致[ICON_UNHAPPY]和[ICON_UNHEALTHY]在你们的城市</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_MISC_CORPORATION_REMOVED</Tag>
+		<English>[COLOR_HIGHLIGHT_TEXT]%s1_CorpName[COLOR_REVERT] has left %s2_CityName.</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>[COLOR_HIGHLIGHT_TEXT]%s1_CorpName[COLOR_REVERT]已经离开了%s2_CityName。</Chinese>
+	</TEXT>	
+	<TEXT>
+		<Tag>TXT_KEY_CORPORATION_INFLUENCES_10</Tag>
+		<English>Attracted by:[NEWLINE][ICON_BULLET]Additional company resources[NEWLINE][ICON_BULLET]Adopting the Free Enterprise civic[NEWLINE][ICON_BULLET]Factories, Laboratories, Universities, CERN Research Complex[NEWLINE][ICON_BULLET]Trade routes[NEWLINE][NEWLINE]Deterred by:[NEWLINE][ICON_BULLET]Competition with the Oil Industry company[NEWLINE][ICON_BULLET]Too many other companies present[NEWLINE][NEWLINE]Side effects:[NEWLINE][ICON_BULLET]Can cause [ICON_UNHAPPY] and [ICON_UNHEALTHY] in your cities</English>
+		<L1/><L2/><L3/><L4/>
+		<Chinese>吸引了：[NEWLINE][ICON_BULLET]更多的公司资源[NEWLINE][ICON_BULLET]采用自由企业公民[NEWLINE][ICON_BULLET]工厂、实验室、大学，欧洲核子研究中心研究中心[NEWLINE][ICON_BULLET]贸易路线[NEWLINE][NEWLINE]被[NEWLINE][ICON_BULLET]与石油工业公司的竞争[NEWLINE][ICON_BULLET]太多其他公司[NEWLINE][NEWLINE]副作用：[NEWLINE][ICON_BULLET]会在你的城市造成[ICON_UNHAPPY]和[ICON_UNHEALTHY] </Chinese>
+	</TEXT>	
+	<TEXT>
+
+	TXT_KEY_CORPORATION_SPICE_ROUTE = {
+		"Spice Route<",
+		"香料之路",
+	},
